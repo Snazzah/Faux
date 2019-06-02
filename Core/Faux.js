4aecae5e46466f0707623a84d1fe71eeb947b9d9
Snazzah <suggesttosnazzy@gmail.com>
2019-06-02 05:10:56 -0500

    Version bump (1.1.0)

--- Core/Faux.js
+++ Core/Faux.js
@@ -9,7 +9,7 @@ const chalk = require('chalk')
 
 module.exports = class Faux extends Discord.Client {
   get FAUX_VER() {
-    return '1.0.1';
+    return '1.1.0';
   }
 
   constructor({configPath, packagePath, mainDir} = {}) {
