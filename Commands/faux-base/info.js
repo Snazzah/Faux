49d7551a14d347de937569d3f82f6a288514a698
Snazzah <suggesttosnazzy@gmail.com>
2019-06-02 04:33:06 -0500

    Fix info command

--- Commands/faux-base/info.js
+++ Commands/faux-base/info.js
@@ -14,7 +14,7 @@ module.exports = class Info extends Command {
                   + `**:computer: Faux Version** ${this.client.FAUX_VER}\n`
                   + `**:clock: Uptime**: ${process.uptime() ? process.uptime().toString().toHHMMSS() : "???"}\n`
                   + `**:gear: Memory Usage**: ${(process.memoryUsage().heapUsed / 1000000).toFixed(2)} MB\n`
-                  + `**:file_cabinet: Servers**: ${servers.reduce((prev, val) => prev + val, 0).formatNumber()}`,
+                  + `**:file_cabinet: Servers**: ${servers.formatNumber()}`,
       thumbnail: {
         url: "https://raw.githubusercontent.com/Snazzah/Faux/master/Assets/icon_blackoutline_color.png"
       }
